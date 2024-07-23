5bd39a33f1c8a5594ffd1748553d4e87ff3ced54
Dhanusha-Chittidi <121395196+Dhanusha-Chittidi@users.noreply.github.com>
2024-07-23 22:30:27 +0530

    Container With Most Water.cpp on Day-6

---
+++ Arrays/Container With Most Water.cpp
@@ -0,0 +1,13 @@
+int maxArea(vector<int>& height) {
+        int n=height.size();
+        int left=0,right=n-1;
+       
+        int ans=0;
+        while(left<right){
+            ans=max(ans,(right-left)*min(height[left],height[right]));
+            if(height[left] < height[right]) left++;
+            else  right--;
+            
+        }
+        return ans;
+    }
